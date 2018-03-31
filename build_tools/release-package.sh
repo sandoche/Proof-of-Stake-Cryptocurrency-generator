7b6d57567fdb186c80ee8dd4dc5e0f77e1d66143
Sandoche Adittane <sandoche@gmail.com>
2018-03-31 16:31:33 +0200

    :bug: Changes to make the build work on Windows

--- build_tools/release-package.sh
+++ build_tools/release-package.sh
@@ -15,7 +15,7 @@ FILES="${FILES} nxt.exe nxtservice.exe"
 FILES="${FILES} 3RD-PARTY-LICENSES.txt AUTHORS.txt LICENSE.txt"
 FILES="${FILES} DEVELOPERS-GUIDE.md OPERATORS-GUIDE.md README.md README.txt USERS-GUIDE.md"
 FILES="${FILES} mint.bat mint.sh run.bat run.sh run-tor.sh run-desktop.sh start.sh stop.sh compact.sh compact.bat sign.sh"
-FILES="${FILES} nxt.policy nxtdesktop.policy NXT_Wallet.url Dockerfile"
+FILES="${FILES} nxt.policy nxtdesktop.policy Wallet.url Dockerfile"
 
 unix2dos *.bat
 echo compile
